0b28968f665d59cab90ec635580ad88a4a58bec3
BurakAy <burak@burak-aydemir.com>
2017-08-31 17:04:27 -0500

    fix user link

--- src/camper_list_item.js
+++ src/camper_list_item.js
@@ -4,7 +4,7 @@ const CamperListItem = ({ camper, number }) => {
     return (
         <tr>
             <td>{number}</td>
-            <td><a href={'https://freecodecamp.org/${camper.username}'} target="_blank"><img src={camper.img} alt="" />{camper.username}</a></td>
+            <td><a href={'https://freecodecamp.org/'+ camper.username} target="_blank"><img src={camper.img} alt="" />{camper.username}</a></td>
             <td>{camper.recent}</td>
             <td>{camper.alltime}</td>
         </tr>
